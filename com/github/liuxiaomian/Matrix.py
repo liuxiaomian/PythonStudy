fde1c5b591d21732e48b5828c90d7ab08e3aff61
liuxiaomian <mian.liu@foxmail.com>
2016-03-14 17:12:33 +0800

    矩阵反转，未写输入

---
+++ com/github/liuxiaomian/Matrix.py
@@ -0,0 +1,14 @@
+
+def convert():
+    line1 = [1,2,3]
+    line2 = [7,8,9]
+
+    line1.__sizeof__()
+
+    for i in range(len(line1)):
+        print(line1[i],end="")
+        print(line2[i])
+    return
+
+convert()
+
